fbd3db8c98abcb99ad23d210e7d7f2a53220d788
frappe <admin@frappe.com.sa>
2023-05-06 18:56:08 +0300

    working on scheduling  logic

--- utils/logics/TimetableScheduling.py
+++ utils/logics/TimetableScheduling.py
@@ -4,15 +4,15 @@
 class TimetableScheduling():
     """Auto & Manual Scheduling
     Auto-scheduling:
-    1- Collect input parameters from the user, such as the doctors' available times and the number of tables to be generated.
-    2- Create a list of all available time slots for each doctor.
-    3- Sort the list of time slots for each doctor in ascending order of time.
+    1- Collect input parameters from the user.
+    2- Create a list of all available time slots for each instructor.
+    3- Sort the list of time slots for each instructor in ascending order of time.
     4- Initialize the schedule with an empty list of tables.
-    5- For each table to be generated, select one doctor from the list of available doctors.
-    6- For the selected doctor, select the earliest available time slot that does not conflict with any already scheduled time slots.
-    7- If a suitable time slot is found, assign the doctor to the table and add the table to the schedule.
-    8- If no suitable time slot is found, try the next doctor in the list.
-    9- If all doctors have been tried and no suitable time slot has been found, reschedule a conflicting table with a lower priority to a later time slot.
+    5- For each table to be generated, select one instructor from the list of available instructors.
+    6- For the selected instructor, select the earliest available time slot that does not conflict with any already scheduled time slots.
+    7- If a suitable time slot is found, assign the instructor to the table and add the table to the schedule.
+    8- If no suitable time slot is found, try the next instructor in the list.
+    9- If all instructors have been tried and no suitable time slot has been found, reschedule a conflicting table with a lower priority to a later time slot.
     10- Repeat steps 5-9 until all tables have been generated and scheduled.
 
 
@@ -20,10 +20,13 @@ class TimetableScheduling():
 
     def __init__(self, *args, **kwargs):
         super(TimetableScheduling, self).__init__(*args, **kwargs)
-        # Collect input parameters from the user
+        self.year = 1 # 1 or 2 or 3 or 4
+        self.language = "ar" # 'ar' or 'en' or 'fr'
+        self.regulation = ""
+        self.department = ""
         self.num_tables = 0 # the number of tables to be generated
-        self.num_doctors = 0 # the number of available doctors
-        self.doctor_availability  = {}
+        self.num_instructors = 0 # the number of available instructors
+        self.instructors_availability  = {} # all instructors with a sorted list for each one with their available times.
 
     #######################################################################################################################
     ### Auto
@@ -32,7 +35,7 @@ class TimetableScheduling():
     def auto_scheduling(self):
         pass
 
-    def get_each_doctor_availability(self):
+    def get_each_instructor_availability(self):
         pass
 
     #######################################################################################################################
