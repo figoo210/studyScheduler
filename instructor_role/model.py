183505a3058d892abad47a55ff99e614f9824756
Abdo-hany15 <abdelrahmanhany536@gmail.com>
2023-05-09 22:06:35 +0300

    make all entities models

---
+++ instructor_role/model.py
@@ -0,0 +1,18 @@
+from database import db
+
+
+class InstructorRole(db.Model):
+    """
+    argument of relationship must be capital like class name 
+    backref can be small because of the model
+    laze prefare to be T
+    in relations 
+    one :make db.relation and add column name s like courses 
+    many: make or put Foreign key on it and name single 
+    Foreign_key=can be small like ("instructor_time.id")
+    """
+    # role  الرتبة
+    name = db.Column(db.String(30), primary_key=True)
+    description = db.Column(db.Text, unique=False, nullable=True)
+    instructors = db.relationship(
+        "Instructor", backref=" instructor", lazy=True)
--- utils/enums/WeekDay.py
+++ utils/enums/WeekDay.py
@@ -2,13 +2,17 @@ from strenum import StrEnum
 from database import db
 
 
-class Language(StrEnum):
-    ar = "ar"
-    en = "en"
-    fr = "fr"
+class WeekDay(StrEnum):
+    sat = "Saturday"
+    sun = "Sunday"
+    mon = "Monday"
+    tue = "Tuesday"
+    wed = "Wednesday"
+    thu = "Thursday"
+    fri = "Friday"
 
 
-class LanguageEnum(db.TypeDecorator):
+class WeekDayEnum(db.TypeDecorator):
     """
     Enables passing in a Python enum and storing the enum's *value* in the db.
     The default would have stored the enum's *name* (ie the string).
@@ -16,7 +20,7 @@ class LanguageEnum(db.TypeDecorator):
     impl = db.String(2)
 
     def __init__(self, enumtype, *args, **kwargs):
-        super(LanguageEnum, self).__init__(*args, **kwargs)
+        super(WeekDayEnum, self).__init__(*args, **kwargs)
         self._enumtype = enumtype
 
     def process_bind_param(self, value, dialect):
