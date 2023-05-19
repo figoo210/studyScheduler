e757f2a19c66fbb6697764c5809b536de34cf26a
Abdo-hany15 <abdelrahmanhany536@gmail.com>
2023-05-19 18:34:18 +0300

    add main routes to intructor view

--- instructor/view.py
+++ instructor/view.py
@@ -5,19 +5,26 @@ PAGE = 'instructor'
 
 inst = Blueprint(PAGE, __name__, template_folder='templates')
 
+# THIS THE MAIN PAGE
 
-@inst.route(f'/{PAGE}/<id>', methods=["GET", "POST"])
-def handle_one(id):
+
+@inst.route(f'/{PAGE}/<id>', methods=["GET"])
+def instructorfunc(id):
     """ get one item or post to update it """
-    context = {}
-    if request.method == 'POST':
-        return render_template(f'{PAGE}.html')
-    else:
-        return render_template(f'{PAGE}.html')
+    return render_template(f'{PAGE}.html')
+
+# THIS FOR POP UP
 
 
-@inst.route(f'/{PAGE}', methods=["GET", "POST"])
-def handle_mod():
+@inst.route(f'/{PAGE}/delete/<id>', methods=["POST"])
+def delete_instrucor_name(id):
+    """ delete item """
+    return redirect(f'/{PAGE}')
+
+
+# ADD INSTRUCTORE
+@inst.route(f'/{PAGE}/<id>', methods=["GET", "POST"])
+def add_instructors(id):
     """ get all items or post to add new """
     context = {}
     if request.method == 'POST':
@@ -25,14 +32,26 @@ def handle_mod():
     else:
         return render_template(f'{PAGE}.html')
 
-
-@inst.route(f'/{PAGE}/update', methods=["POST"])
-def update():
-    """ post to update """
-    return redirect(f'/{PAGE}')
+# Designation of materials
 
 
-@inst.route(f'/{PAGE}/delete/<id>', methods=["GET"])
-def delete_one(id):
-    """ delete item """
-    return redirect(f'/{PAGE}')
+@inst.route(f'/{PAGE}/<id>', methods=["GET", "POST"])
+def designation_materials(id):
+    """ get all items or post to add new """
+    context = {}
+    if request.method == 'POST':
+        return render_template(f'{PAGE}.html')
+    else:
+        return render_template(f'{PAGE}.html')
+
+# Profile personly
+
+
+@inst.route(f'/{PAGE}/<id>', methods=["GET", "POST"])
+def profile_personly(id):
+    """ get all items or post to add new """
+    context = {}
+    if request.method == 'POST':
+        return render_template(f'{PAGE}.html')
+    else:
+        return render_template(f'{PAGE}.html')
