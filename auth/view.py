64b84fb7abd839ec740ec1c4c4e1025fae8c0e78
Abdo-hany15 <abdelrahmanhany536@gmail.com>
2023-05-20 23:45:37 +0300

    add route to auth

--- auth/view.py
+++ auth/view.py
@@ -1,19 +1,32 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, request
 from auth.forms import AuthForm
 from auth.controller import welcome_to_dash
 
 PAGE = 'auth'
 
-aut = Blueprint(PAGE, __name__, template_folder='templates')
+bp = Blueprint(PAGE, __name__, template_folder='templates')
+
+# login page
 
 
-@aut.route(f'/{PAGE}', methods=['POST'])
-def submit():
-    form = AuthForm()
-    if form.validate_on_submit():
-        # change username and password
-        welcome_to_dash(form)
-        return render_template(f'{PAGE}.html')  # 'Welcome to the dashboard'
+@bp.route(f'/login/<id>', methods=["GET", "POST"])
+def login_page(id):
+    """ sign in  """
+    context = {}
+    if request.method == 'POST':
+        # return  changes(main_view.py)
+        return render_template(f'{PAGE}.html')
     else:
-        # 'Invalid username or password'
+        return render_template(f'{PAGE}.html')
+
+
+# forget password
+@bp.route(f'/verification/<id>', methods=["GET", "POST"])
+def verification(id):
+    """ sign in  """
+    context = {}
+    if request.method == 'POST':
+        # return  changes
+        return redirect(f'/{PAGE}/{id}')
+    else:
         return render_template(f'{PAGE}.html')
