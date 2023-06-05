956d0e1823bfc8a1282daaf9dece558a0c58d2ed
Abdo-hany15 <abdelrahmanhany536@gmail.com>
2023-06-06 01:54:17 +0300

    update controller in all modules except dashboard with check form

--- building/controller.py
+++ building/controller.py
@@ -29,3 +29,20 @@ def get_buildings():
 
 
 
+def is_form_empty(AuthForm):
+    # Loop through all form fields
+    for field in AuthForm.values():
+        # Check if the field has a value
+        if field.strip() != '':
+            flash("لم يتم الحفظ")
+            return False  # Form is not empty
+        flash("تم الحفظ")
+    return True  # Form is empty
+
+"""
+# To use this function
+check = is_form_empty(AuthForm)
+# True if form is empty, False otherwise
+print(check)
+
+"""
--- course/controller.py
+++ course/controller.py
@@ -1,5 +1,24 @@
+from flask import flash
 from course.model import Course
 
 
 def get_all_courses():
     return Course.query.all()
+
+def is_form_empty(AuthForm):
+    # Loop through all form fields
+    for field in AuthForm.values():
+        # Check if the field has a value
+        if field.strip() != '':
+            flash("لم يتم الحفظ")
+            return False  # Form is not empty
+        flash("تم الحفظ")
+    return True  # Form is empty
+
+"""
+# To use this function
+check = is_form_empty(AuthForm)
+# True if form is empty, False otherwise
+print(check)
+
+"""
